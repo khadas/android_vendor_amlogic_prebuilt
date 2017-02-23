b29d25a683ce7c960421d934b3f763df6a38adf9
Kieth Liu <kieth.liu@amlogic.com>
2017-02-23 10:31:08 +0800

    PD#139868: no need tv prebuilt libs when have tvserver source code
    Change-Id: I76973062917131cfe47ac5e9adc7e43ece2c51f0

--- tv/Android.mk
+++ tv/Android.mk
@@ -1,6 +1,10 @@
 LOCAL_PATH := $(call my-dir)
-
 include $(CLEAR_VARS)
+
+ifeq ($(wildcard vendor/amlogic/tvserver/Android.mk),vendor/amlogic/tvserver/Android.mk)
+$(info "have tvserver source code")
+else
+$(info "no tvserver source code, add binary lib")
 LOCAL_MODULE_TAGS := optional
 LOCAL_PREBUILT_LIBS := libtv.so
 include $(BUILD_MULTI_PREBUILT)
@@ -18,3 +22,4 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_EXECUTABLES)
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
 
+endif
