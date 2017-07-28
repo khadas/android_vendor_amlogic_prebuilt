ca250a058ff8e59aaa7e80edb028d43bfaa71bad
Kieth Liu <kieth.liu@amlogic.com>
2017-07-28 11:06:56 +0800

    tvserver: update tv binarys[1/1]
    PD# 148081
    
    update tv binarys from e3d0327211016f64611058508d076c32289eea12
    that PD#148081 modify
    
    Change-Id: I224e7e445fb63e7f46c935ee180466147091fe5f

--- tv/Android.mk
+++ tv/Android.mk
@@ -1,33 +1,55 @@
 LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
+multilib := both
 
 ifeq ($(wildcard vendor/amlogic/tv/tvserver/Android.mk),vendor/amlogic/tv/tvserver/Android.mk)
 $(info "have tvserver source code")
 else
 $(info "no tvserver source code, add binary lib")
+include $(CLEAR_VARS)
+LOCAL_MODULE := libtv
+LOCAL_MODULE_SUFFIX :=.so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_TAGS := optional
-LOCAL_PREBUILT_LIBS := libtv.so
-ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 26 && echo OK),OK)
-LOCAL_PROPRIETARY_MODULE := true
+LOCAL_PRELINK_MODULE := false
+ifdef TARGET_2ND_ARCH
+LOCAL_MULTILIB := $(multilib)
+LOCAL_MODULE_PATH_64 := $(TARGET_OUT)/lib64
+LOCAL_SRC_FILES_64 := 64bit/lib64/libtv.so
+LOCAL_MODULE_PATH_32 := $(TARGET_OUT)/lib
+LOCAL_SRC_FILES_32 := 64bit/lib/libtv.so
+else
+LOCAL_MODULE_PATH := $(TARGET_OUT)/lib
+LOCAL_SRC_FILES := 32bit/libtv.so
 endif
-include $(BUILD_MULTI_PREBUILT)
+include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := libtv_linker
+LOCAL_MODULE_SUFFIX :=.so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_TAGS := optional
-LOCAL_PREBUILT_LIBS := libtv_linker.so
-ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 26 && echo OK),OK)
-LOCAL_PROPRIETARY_MODULE := true
+LOCAL_PRELINK_MODULE := false
+ifdef TARGET_2ND_ARCH
+LOCAL_MULTILIB := $(multilib)
+LOCAL_MODULE_PATH_64 := $(TARGET_OUT)/lib64
+LOCAL_SRC_FILES_64 := 64bit/lib64/libtv_linker.so
+LOCAL_MODULE_PATH_32 := $(TARGET_OUT)/lib
+LOCAL_SRC_FILES_32 := 64bit/lib/libtv_linker.so
+else
+LOCAL_MODULE_PATH := $(TARGET_OUT)/lib
+LOCAL_SRC_FILES := 32bit/libtv_linker.so
 endif
-include $(BUILD_MULTI_PREBUILT)
+include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := tvserver
 LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_MODULE_PATH := $(TARGET_OUT_EXECUTABLES)
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 26 && echo OK),OK)
-LOCAL_PROPRIETARY_MODULE := true
+ifdef TARGET_2ND_ARCH
+LOCAL_SRC_FILES := 64bit/tvserver
+else
+LOCAL_SRC_FILES := 32bit/tvserver
 endif
 include $(BUILD_PREBUILT)
 
