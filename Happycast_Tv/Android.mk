3e4e55f93e7a11c169d22edc749fad5dcdd39a41
Zhiwei Gong <zhiwei.gong@amlogic.com>
2017-10-23 14:43:34 +0800

    Update ylt apk [2/3]
    PD# 146643
    
    Update Happycast_Tv.apk to version 6710 for support android 8.0&ios11
    
    Change-Id: I77665fde323ad43843daf0a7b0572457d0793320

--- Happycast_Tv/Android.mk
+++ Happycast_Tv/Android.mk
@@ -3,29 +3,10 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE:= Happycast_Tv
 LOCAL_MODULE_CLASS := APPS
-LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_SUFFIX := .apk
+LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := Happycast_Tv.apk
-LOCAL_PREBUILT_JNI_LIBS := \
-    lib/armeabi-v7a/libhpplaysmdns.so \
-    lib/armeabi-v7a/libhpplayvideo19.so \
-    lib/armeabi-v7a/libMtaNativeCrash.so \
-    lib/armeabi-v7a/libamlogiccast.so \
-    lib/armeabi-v7a/libdbapi.so \
-    lib/armeabi-v7a/libdownloadinstall.so \
-    lib/armeabi-v7a/libhisivideo19.so \
-    lib/armeabi-v7a/libhisivideo_3798m.so \
-    lib/armeabi-v7a/libhisivideo.so \
-    lib/armeabi-v7a/libhpplayaudio.so \
-    lib/armeabi-v7a/libhpplaymdns.so \
-    lib/armeabi-v7a/libhpplaymirror.so \
-    lib/armeabi-v7a/libhpplayvideo.so \
-    lib/armeabi-v7a/liblbffmpeg.so \
-    lib/armeabi-v7a/liblbplayer.so \
-    lib/armeabi-v7a/liblbsdl.so \
-    lib/armeabi-v7a/liblebodlna-jni.so \
-    lib/armeabi-v7a/libndkbitmap.so
-LOCAL_MULTILIB := 32
+LOCAL_MODULE_TARGET_ARCH := arm
 ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 26 && echo OK),OK)
 LOCAL_PROPRIETARY_MODULE := true
 endif
